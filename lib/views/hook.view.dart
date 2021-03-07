2b69e25c5f969292c3bac2c49f51a22bdf628872
Nour <nour.eldin.shobier@gmail.com>
2021-03-07 18:44:11 +0200

    fix: pass key object to super widgets

--- lib/views/hook.view.dart
+++ lib/views/hook.view.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:pmvvm/pmvvm.dart';
 
 abstract class HookView<T> extends HookWidget {
-  const HookView({Key? key, this.reactive = true});
+  const HookView({Key? key, this.reactive = true}) : super(key: key);
 
   final bool reactive;
 
   @override
-  Widget build(context) =>
-      render(context, Provider.of<T>(context, listen: reactive));
+  Widget build(context) => render(context, Provider.of<T>(context, listen: reactive));
 
   Widget render(BuildContext context, T viewModel);
 }
--- lib/views/stateless.view.dart
+++ lib/views/stateless.view.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:pmvvm/pmvvm.dart';
 
 abstract class StatelessView<T> extends StatelessWidget {
-  const StatelessView({Key? key, this.reactive = true});
+  const StatelessView({Key? key, this.reactive = true}) : super(key: key);
 
   final bool reactive;
 
   @override
-  Widget build(context) =>
-      render(context, Provider.of<T>(context, listen: reactive));
+  Widget build(context) => render(context, Provider.of<T>(context, listen: reactive));
 
   Widget render(BuildContext context, T viewModel);
 }
